4bb64a0aa0bcce8f668fda9537dd48186bc5a1d1
Fuadi <fuadih3@gmail.com>
2025-07-09 19:42:05 +0930

    update error Location v6

--- resources/views/components/form/file.blade.php
+++ resources/views/components/form/file.blade.php
@@ -1,19 +1,33 @@
 @props([
-    'id',
+    'id' => null,
     'name',
-    'label',
+    'label' => '',
     'accept' => '',
+    'required' => false,
 ])
 
-<div>
-    <label for="{{ $id }}" class="block text-sm font-semibold text-gray-700 mb-1">{{ $label }}</label>
+@php
+    $id = $id ?? $name;
+@endphp
+
+<div class="mb-4 {{ $attributes->get('class') }}">
+    @if ($label)
+        <label for="{{ $id }}" class="block text-sm font-semibold text-gray-700 mb-1">
+            {{ $label }}
+        </label>
+    @endif
+
     <input
         type="file"
-        name="{{ $name }}"
         id="{{ $id }}"
+        name="{{ $name }}"
         accept="{{ $accept }}"
-        class="mt-1 block w-full text-sm text-gray-500 file:py-2 file:px-4 file:border-0 file:rounded-md file:bg-teal-50 file:text-teal-700 hover:file:bg-teal-100"
-    >
+        {{ $required ? 'required' : '' }}
+        {{ $attributes->except('class')->merge([
+            'class' => 'block w-full text-sm text-gray-700 file:mr-4 file:py-2 file:px-4 file:rounded file:border-0 file:text-sm file:font-semibold file:bg-teal-50 file:text-teal-700 hover:file:bg-teal-100'
+        ]) }}
+    />
+
     @error($name)
         <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
     @enderror
