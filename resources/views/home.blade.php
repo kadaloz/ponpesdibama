0479d6a4dfbbe765b3e11c496ae039bf650b26b1
Fuadi <fuadih3@gmail.com>
2025-07-13 15:45:26 +0930

    update icon logo dibama v2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,19 +17,20 @@
     <div class="relative z-10 text-center px-6 max-w-4xl mx-auto animate-fade-in-up">
     
 <!-- Logo Institusi -->
-<div class="flex justify-center items-center gap-3 mb-6">
+<div class="flex justify-center items-center gap-2 mb-4">
     <img src="{{ asset('storage/images/logo/academy.webp') }}"
          alt="Logo Academy"
-         class="max-w-[40px] h-auto scale-90 sm:scale-100 md:scale-100 rounded-md shadow-md hover:scale-105 transition-transform duration-300 ease-in-out">
+         class="max-w-[28px] h-auto scale-90 sm:max-w-[32px] md:max-w-[36px] lg:max-w-[40px] rounded-md shadow-md hover:scale-105 transition-transform duration-300 ease-in-out">
 
     <img src="{{ asset('storage/images/logo/diniyah.webp') }}"
          alt="Logo Diniyah"
-         class="max-w-[40px] h-auto scale-90 sm:scale-100 md:scale-100 rounded-md shadow-md hover:scale-105 transition-transform duration-300 ease-in-out">
+         class="max-w-[28px] h-auto scale-90 sm:max-w-[32px] md:max-w-[36px] lg:max-w-[40px] rounded-md shadow-md hover:scale-105 transition-transform duration-300 ease-in-out">
 
     <img src="{{ asset('storage/images/logo/ponpes.webp') }}"
          alt="Logo Ponpes"
-         class="max-w-[40px] h-auto scale-90 sm:scale-100 md:scale-100 rounded-md shadow-md hover:scale-105 transition-transform duration-300 ease-in-out">
+         class="max-w-[28px] h-auto scale-90 sm:max-w-[32px] md:max-w-[36px] lg:max-w-[40px] rounded-md shadow-md hover:scale-105 transition-transform duration-300 ease-in-out">
 </div>
+
 
 
 
