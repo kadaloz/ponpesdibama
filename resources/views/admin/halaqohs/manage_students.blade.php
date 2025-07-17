b384348a9e54437e94f7598381f5c9a3f2d2ce66
Fuadi <fuadih3@gmail.com>
2025-07-17 12:42:38 +0930

    Update Halaqoh Santri menggunakan API

--- resources/views/admin/halaqohs/manage_students.blade.php
+++ resources/views/admin/halaqohs/manage_students.blade.php
@@ -77,7 +77,7 @@ document.addEventListener("DOMContentLoaded", function () {
         placeholder: 'Ketik nama atau NIS santri...',
         load: function(query, callback) {
             if (!query.length) return callback();
-            fetch(`/api/students/search?q=${encodeURIComponent(query)}`)
+            fetch(`admin/api/students/search?q=${encodeURIComponent(query)}`)
                 .then(response => response.json())
                 .then(callback)
                 .catch(() => callback());
