67daebfd1642ea774e7b0cfd06f8118458e8b9ea
Fuadi <fuadih3@gmail.com>
2025-07-18 20:37:29 +0930

    Tambah Inventaris

--- resources/views/admin/rooms/show.blade.php
+++ resources/views/admin/rooms/show.blade.php
@@ -72,12 +72,12 @@
                 @else
                     <p class="text-gray-600 italic">Belum ada santri yang menempati kamar ini.</p>
                 @endif
-                <div class="mt-4 text-right">
-                     <a href="{{ route('admin.rooms.assign.form', $room) }}" class="inline-flex items-center px-4 py-2 bg-purple-600 border border-transparent rounded-full font-semibold text-xs text-white uppercase tracking-widest hover:bg-purple-700 focus:bg-purple-700 active:bg-purple-800 focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-offset-2 transition ease-in-out duration-150 shadow-md">
-                        <x-heroicon-o-user-plus class="w-4 h-4 mr-2 -ml-1" />
-                        Atur Penghuni
-                    </a>
-                </div>
+<div class="mt-4 text-right">
+     <a href="{{ route('admin.rooms.assign.form', $room) }}" class="inline-flex items-center px-4 py-2 bg-purple-600 border border-transparent rounded-full font-semibold text-xs text-white uppercase tracking-widest hover:bg-purple-700 focus:bg-purple-700 active:bg-purple-800 focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-offset-2 transition ease-in-out duration-150 shadow-md">
+        <x-heroicon-o-user-plus class="w-4 h-4 mr-2 -ml-1" />
+        Atur Penghuni
+    </a>
+</div>
             </div>
 
 
