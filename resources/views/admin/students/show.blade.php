e97430c0f8cce8956061fc0caf4a9dd7bfb20edf
Fuadi <fuadih3@gmail.com>
2025-07-22 15:35:35 +0930

    update foto Profil untuk guru

--- resources/views/admin/students/show.blade.php
+++ resources/views/admin/students/show.blade.php
@@ -177,17 +177,21 @@
                     </div>
                 @endif
 
-                {{-- Tombol Aksi --}}
-                <div class="mt-10 text-center border-t pt-6 flex justify-end space-x-4">
-                    <a href="{{ route('admin.students.index') }}" class="inline-flex items-center px-6 py-3 bg-gray-200 border border-transparent rounded-full font-semibold text-xs text-gray-700 uppercase tracking-widest hover:bg-gray-300 focus:bg-gray-300 active:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-offset-2 transition ease-in-out duration-150 shadow-md">
-                        <x-heroicon-o-arrow-left class="w-4 h-4 mr-2 -ml-1" />
-                        Kembali ke Daftar
-                    </a>
-                    <a href="{{ route('admin.students.edit', $student) }}" class="inline-flex items-center text-sm px-4 py-2 bg-teal-600 border border-transparent rounded-full font-semibold text-white uppercase tracking-widest hover:bg-teal-700 focus:bg-teal-700 active:bg-teal-800 focus:outline-none focus:ring-2 focus:ring-teal-400 focus:ring-offset-2 transition ease-in-out duration-150 shadow-md">
-                        <x-heroicon-o-pencil-square class="w-4 h-4 mr-2 -ml-1" />
-                        Edit Santri
-                    </a>
-                </div>
+{{-- Tombol Aksi --}}
+<div class="mt-10 text-center border-t pt-6 flex justify-end space-x-4">
+    <a href="{{ route('admin.students.index') }}" class="inline-flex items-center px-6 py-3 bg-gray-200 border border-transparent rounded-full font-semibold text-xs text-gray-700 uppercase tracking-widest hover:bg-gray-300 focus:bg-gray-300 active:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-offset-2 transition ease-in-out duration-150 shadow-md">
+        <x-heroicon-o-arrow-left class="w-4 h-4 mr-2 -ml-1" />
+        Kembali ke Daftar
+    </a>
+
+    @can('edit students')
+    <a href="{{ route('admin.students.edit', $student) }}" class="inline-flex items-center text-sm px-4 py-2 bg-teal-600 border border-transparent rounded-full font-semibold text-white uppercase tracking-widest hover:bg-teal-700 focus:bg-teal-700 active:bg-teal-800 focus:outline-none focus:ring-2 focus:ring-teal-400 focus:ring-offset-2 transition ease-in-out duration-150 shadow-md">
+        <x-heroicon-o-pencil-square class="w-4 h-4 mr-2 -ml-1" />
+        Edit Santri
+    </a>
+    @endcan
+</div>
+
             </div>
         </div>
     </div>
