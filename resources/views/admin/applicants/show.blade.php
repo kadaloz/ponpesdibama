135b5e28ef7396b53716eeb1c9456e55ef62ddf1
Fuadi <fuadih3@gmail.com>
2025-07-25 16:30:04 +0930

    Update assign

--- resources/views/admin/applicants/show.blade.php
+++ resources/views/admin/applicants/show.blade.php
@@ -22,21 +22,21 @@
                     <dd class="text-xl mt-1">{{ $applicant->full_name }}</dd>
                 </div>
                 <div>
-    <dt class="font-semibold text-gray-600">Program Dipilih:</dt>
-    @php
-        $program = $applicant->chosen_program;
-        $programClass = match ($program) {
-            'Tahfizh' => 'bg-green-100 text-green-800',
-            'Kitab Kuning' => 'bg-yellow-100 text-yellow-800',
-            'Bahasa Arab' => 'bg-blue-100 text-blue-800',
-            default => 'bg-gray-100 text-gray-800'
-        };
-    @endphp
-    <dd class="mt-1">
-        <span class="inline-block px-4 py-2 rounded-full font-semibold text-sm {{ $programClass }}">
-            {{ $program ?? 'Tidak Ada' }}
-        </span>
-    </dd>
+   <dt class="font-semibold text-gray-600">Program Dipilih:</dt>
+@php
+    $programName = $applicant->program->name ?? null;
+    $programClass = match ($programName) {
+        'Tahfizh' => 'bg-green-100 text-green-800',
+        'Kitab Kuning' => 'bg-yellow-100 text-yellow-800',
+        'Bahasa Arab' => 'bg-blue-100 text-blue-800',
+        default => 'bg-gray-100 text-gray-800'
+    };
+@endphp
+<dd class="mt-1">
+    <span class="inline-block px-4 py-2 rounded-full font-semibold text-sm {{ $programClass }}">
+        {{ $programName ?? 'Tidak Ada' }}
+    </span>
+</dd>
 </div>
 
 <div>
