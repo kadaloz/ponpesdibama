198e1d81968f406cbea6e94f63e2730ad531a411
Fuadi <fuadih3@gmail.com>
2025-07-27 16:38:06 +0930

    update item

--- resources/views/admin/students/index.blade.php
+++ resources/views/admin/students/index.blade.php
@@ -75,7 +75,7 @@
                 </div>
 
                 {{-- Tombol --}}
-                <div class="flex justify-between flex-wrap gap-2 md:col-span-5 mt-4">
+                <div class="flex justify-between flex-wrap gap-1 md:col-span-5 mt-4">
                     <div class="flex gap-2">
                         <button type="submit" class="inline-flex items-center px-4 py-2 bg-teal-600 text-white text-sm font-medium rounded-lg hover:bg-teal-700 transition">
                             <x-heroicon-o-magnifying-glass class="w-5 h-5 mr-2" /> Filter
