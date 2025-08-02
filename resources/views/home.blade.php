c5518ab960cc8650e20ae64408db123cce4c1636
Fuadi <fuadih3@gmail.com>
2025-08-02 20:48:37 +0930

    update entry year

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -109,7 +109,7 @@
                     <div class="swiper-wrapper">
                         @php dd(get_defined_vars()); @endphp
 
-                        @forelse ($photos->take(5) as $photo)
+                        @forelse ($pondokPhoto->take(5) as $photo)
                             <div class="swiper-slide">
                                 <img src="{{ asset('storage/' . $photo) }}" alt="Foto Pondok" class="w-full h-full object-cover" />
                             </div>
--- routes/web.php
+++ routes/web.php
@@ -65,9 +65,6 @@ Route::get('/', function () {
     $contactPhone        = $settings['contact_phone'] ?? '+62 812-3456-7890';
     $contactEmail        = $settings['contact_email'] ?? 'info@ponpesdibama.com';
     $pondokPhoto         = collect(json_decode($settings['pondok_photos'] ?? '[]') ?: []);
-    $photos              = collect($pondokPhoto)->filter(function ($item) {
-        return is_string($item) && !empty($item);
-    })->values();
 
 
     $locationMapUrl = $settings['location_map_url'] ?? 'https://www.google.com/maps/embed?...';
@@ -88,7 +85,6 @@ Route::get('/', function () {
         'contactPhone',
         'contactEmail',
         'pondokPhoto',
-        'photos',
         'locationMapUrl',
         'isPpdbOpen',
         'ppdbAcademicYear',
