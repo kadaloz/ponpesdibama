91d77cb811762f375f314952d81ecaded10e776c
Fuadi <fuadih3@gmail.com>
2025-08-02 17:17:20 +0930

    update entry year

--- resources/views/web/footer.blade.php
+++ resources/views/web/footer.blade.php
@@ -15,7 +15,16 @@
             <div>
                 <h5 class="text-lg font-semibold mb-3">Tautan Penting</h5>
                 <ul class="space-y-2 text-sm">
-                    <li><a href="{{ url('/ppdb/daftar') }}" class="hover:underline text-gray-300 hover:text-white transition">Pendaftaran PPDB</a></li>
+@if ($ppdbIsActive)
+    <li>
+        <a href="{{ url('/ppdb/daftar') }}" class="hover:underline text-gray-300 hover:text-white transition">
+            Pendaftaran PPDB
+        </a>
+    </li>
+@else
+    <li class="text-gray-500 cursor-not-allowed">Pendaftaran PPDB (Ditutup)</li>
+@endif
+
                     <li><a href="#" class="hover:underline text-gray-300 hover:text-white transition">Kebijakan Privasi</a></li>
                     <li><a href="#" class="hover:underline text-gray-300 hover:text-white transition">Syarat & Ketentuan</a></li>
                 </ul>
