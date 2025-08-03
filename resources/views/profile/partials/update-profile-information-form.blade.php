05cc8acd6a0785ce505ee40b899f105f99b0cc5c
Fuadi <fuadih3@gmail.com>
2025-08-03 20:25:46 +0930

    buat home controller

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -29,8 +29,8 @@
             <x-input-label for="photo" :value="__('Profile Photo')" />
             <div class="mt-2 flex items-center gap-4">
                 <div class="flex-shrink-0">
-                    @if ($user->profile_photo_path)
-                        <img src="{{ asset('storage/' . $user->profile_photo_path) }}" alt="Profile Photo" class="h-20 w-20 rounded-full object-cover">
+                    @if ($user->photo_path)
+                        <img src="{{ asset('storage/' . $user->photo_path) }}" alt="Profile Photo" class="h-20 w-20 rounded-full object-cover">
                     @else
                         <svg class="h-20 w-20 text-gray-300" fill="currentColor" viewBox="0 0 24 24">
                             <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 1.486 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
